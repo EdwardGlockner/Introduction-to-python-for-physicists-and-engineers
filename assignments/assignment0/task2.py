5430b16305d6309977fa85f80babe54b6955bf93
Edward Glockner <edward.glockner5@gmail.com>
2023-07-02 13:20:45 +0200

    assignment 0 and 1

---
+++ assignments/assignment0/task2.py
@@ -0,0 +1,6 @@
+important_stuff = ["the SciPy library", "solutions for non-linear equations", 
+                   "least squares approximation", "numerical integration", "approximation of data"]
+phrase = "I will learn something about"
+
+for stuff in important_stuff:
+    print(f"{phrase} {stuff}")
